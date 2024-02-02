7ed074cb6cc170bbe40d428f932c29158c6313ac
onkarmandhareomii <155937950+onkarmandhareomii@users.noreply.github.com>
2024-02-02 12:55:56 +0530

    Add files via upload

---
+++ Landing page1/javascript.js
@@ -0,0 +1,9 @@
+let food1 = document.getElementById('food1');
+let food2 = document.getElementById('food2');
+let food = document.getElementById('food');
+food1.addEventListener('click',()=>{
+    food.style.backgroundImage="url('VIDEO/i1.jpeg')"
+})
+food2.addEventListener('click',()=>{
+    food.style.backgroundImage="url('VIDEO/i2.jpeg')"
+})
